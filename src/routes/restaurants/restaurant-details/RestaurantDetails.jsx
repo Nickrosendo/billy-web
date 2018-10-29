58f8da88b481015b4652a99c96d05d287ae07bc4
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2018-10-29 01:04:13 -0300

    style restaurant detail

--- src/routes/restaurants/restaurant-details/RestaurantDetails.jsx
+++ src/routes/restaurants/restaurant-details/RestaurantDetails.jsx
@@ -2,29 +2,33 @@ import { h } from 'preact';
 import style from './style';
 
 const RestaurantDetails = (props, state) => {
+	const menuItemsMap = props.restaurant.menu.map(item => (
+		<div class={style.menuItemContainer}>
+			<p class={style.menuItemName}>
+				{item.name}
+			</p>
+			<img class={style.menuItemImg} src={item.img} />
+			<div class={style.menuItemData}>
+				<p class={style.menuItemDataPrice} ><i class="icon-coin-dollar" /> R$: <span>	{item.price} </span></p>
+				<div class={style.menuItemDataIngredients} >
+					<p class={style.menuItemDataIngredientsTitle}><i class="icon-spoon-knife" /> Ingredientes: </p>
+					{item.ingredients.map(i => (
+						<p> {i}</p>
+					))}
+				</div>
+				<button class={style.menuItemDataOrder}>
+					Pedir
+				</button>
+			</div>
+		</div>
+	));
+
 	return (
-		<div class={style.detailContainer } style="padding-top: 15px;">
+		<div class={style.detailContainer}>
 			<h1 class="text-center">
 				{props.restaurant.name}
 			</h1>
-			<div style="border: 1px solid #ddd; border-radius: 8px; padding: 8px; margin: 15px 0; text-align: center;">
-				<p style="background: #563d7c; color: #fff; font-weight: 600; padding: 8px; margin: 8px 0; text-align: center; border-radius: 12px;">
-					{props.restaurant.menu[0].name}
-				</p>
-				<img style="width: 50%; height: 100px; border-radius: 8px; display: inline-block;" src={props.restaurant.menu[0].img} />
-				<div style="margin: 10px 0;">
-					<p style="font-size: 18px; color: #444; font-weight: 600;"><i class="icon-coin-dollar" /> R$: <span>	{props.restaurant.menu[0].price} </span></p>
-					<div style="padding: 8px; background: #ddd; border-radius: 6px; margin-top: 10px; text-align: left;">
-						<p style="font-size: 16px; color: #444; font-weight: 600;"><i class="icon-spoon-knife" /> Ingredientes: </p>
-						{props.restaurant.menu[0].ingredients.map(i => (
-							<p> {i}</p>
-						))}
-					</div>
-					<button style="padding: 6px; border-radius: 6px; background: #28a745; color: #fff; border: none; margin-top: 10px; display: block; width: 100%;">
-						Pedir
-					</button>
-				</div>
-			</div>
+			{menuItemsMap}
 		</div>
 	);
 };

4776870fad823405fd39b322de002c4727c223c9
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2019-01-09 00:29:58 -0200

    layout improovs

--- src/routes/order/orders-list/OrdersList.jsx
+++ src/routes/order/orders-list/OrdersList.jsx
@@ -1,5 +1,6 @@
 import { h } from 'preact';
 import { connect } from 'preact-redux';
+import { Link } from 'preact-router/match';
 
 import OrdersListItem from './OrdersListItem.jsx';
 
@@ -7,13 +8,29 @@ import style from './style.css';
 
 const OrdersList=connect(state => state)((props, state) => {
 	const { orders }=props;
+	// const orders = [];
+	const ordersMap=() => orders.map(item => (
+		<OrdersListItem order={item} />
+	));
+	const noOrders=() => (
+		<li class={style.noOrdersContainer} >
+			<p class={style.noOrdersIcon} >
+				<i class="icon-edit" />
+			</p>
+			<p>Você não possui nenhum pedido!</p>
+			<Link href={'/restaurantes'} class={style.noOrdersBtn} >
+				Buscar restaurantes
+			</Link>
+		</li>
+	);
+
 	return (
 		<div>
-			<h1 style="text-align: center;">Histórico de pedidos</h1>
+			<h1 style="text-align: center; font-weight: 500;">Histórico de pedidos</h1>
 			<ul class={style.ordersList}>
-				{orders.map(item => (
-					<OrdersListItem order={item} />
-				))}
+				{
+					orders.length>0? ordersMap():noOrders()
+				}
 			</ul>
 		</div>
 	);
